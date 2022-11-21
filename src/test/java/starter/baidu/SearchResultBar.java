8a495be9d51692d2fa63fce8ac5f019be2cd28c1
furichan <1379485872@qq.com>
2022-11-21 21:33:17 +0800

    update code

--- src/test/java/starter/baidu/SearchResultBar.java
+++ src/test/java/starter/baidu/SearchResultBar.java
@@ -7,8 +7,7 @@ public class SearchResultBar extends PageComponent {
     public String heading(){
 //        String result = $("//div[@tpl='open_source_software']//em").getText();
         waitABit(3000);
-        String result = getDriver().findElement(By.xpath("//div[@tpl='open_source_software']//em")).getText();
-        return result;
+        return getDriver().findElement(By.xpath("//div[@tpl='open_source_software']//em")).getText();
 
     }
 }
